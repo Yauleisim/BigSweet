c62fc2c14ff55a975787b5fa7afb82f8fccbf327
qiulichan <s_qiulichan@kingsoft.com>
2019-01-19 17:53:08 +0800

    底部导航栏绑定各Fragment

--- app/src/main/java/com/graduation/yau/bigsweet/MainActivity.java
+++ app/src/main/java/com/graduation/yau/bigsweet/MainActivity.java
@@ -1,13 +1,84 @@
 package com.graduation.yau.bigsweet;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    protected ViewPager mShiftViewPager;
+    private ShiftViewPagerAdapter mShiftViewPagerAdapter;
+    protected BottomNavigationView mBottomNavigationView;
+    protected MenuItem mTabMenuItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+        initEvent();
     }
+
+    private void initView() {
+        mShiftViewPager = findViewById(R.id.shift_main_viewPager);
+        mBottomNavigationView = findViewById(R.id.tab_main_bottomNavigationView);
+    }
+
+    private void initEvent() {
+        mShiftViewPagerAdapter = new ShiftViewPagerAdapter(getSupportFragmentManager());
+        mShiftViewPagerAdapter.addFragment(new HomeFragment());
+        mShiftViewPagerAdapter.addFragment(new ShopFragment());
+        mShiftViewPagerAdapter.addFragment(new PersonFragment());
+
+        mShiftViewPager.setAdapter(mShiftViewPagerAdapter);
+        mShiftViewPager.addOnPageChangeListener(mOnPageChangeListener);
+
+        mBottomNavigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    private ViewPager.OnPageChangeListener mOnPageChangeListener = new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int i, float v, int i1) {
+
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+            if (mTabMenuItem != null) {
+                mTabMenuItem.setChecked(false);
+            } else {
+                mBottomNavigationView.getMenu().getItem(0).setChecked(false);
+            }
+            mTabMenuItem = mBottomNavigationView.getMenu().getItem(position);
+            mTabMenuItem.setChecked(true);
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int i) {
+
+        }
+    };
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            mTabMenuItem = menuItem;
+            switch (menuItem.getItemId()) {
+                case R.id.navigation_home:
+                    mShiftViewPager.setCurrentItem(0);
+                    return true;
+                case R.id.navigation_shop:
+                    mShiftViewPager.setCurrentItem(1);
+                    return true;
+                case R.id.navigation_person:
+                    mShiftViewPager.setCurrentItem(2);
+                    return true;
+            }
+            return false;
+        }
+    };
 }
---
+++ app/src/main/java/com/graduation/yau/bigsweet/ShiftViewPagerAdapter.java
@@ -0,0 +1,34 @@
+package com.graduation.yau.bigsweet;
+
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ShiftViewPagerAdapter extends FragmentStatePagerAdapter {
+
+    private List<Fragment> mFragmentList = new ArrayList<>();
+
+    @Override
+    public Fragment getItem(int position) {
+        return mFragmentList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return null == mFragmentList ? 0 : mFragmentList.size();
+    }
+
+    public ShiftViewPagerAdapter(FragmentManager fragmentManager) {
+        super(fragmentManager);
+    }
+
+    public void addFragment(Fragment fragment) {
+        mFragmentList.add(fragment);
+    }
+
+
+}
