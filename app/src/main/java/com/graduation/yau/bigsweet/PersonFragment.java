54050759effb8fbb561041483038d25acd826d9c
Yauleisim <13631445055@qq.com>
2019-03-31 14:58:12 +0800

    个人页增添未登录状态

--- app/src/main/java/com/graduation/yau/bigsweet/PersonFragment.java
+++ app/src/main/java/com/graduation/yau/bigsweet/PersonFragment.java
@@ -8,14 +8,18 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class PersonFragment extends Fragment implements View.OnClickListener {
+
+    private TextView mUserNameTextView, mSignatureTextView, mSignOutStateTextView, mSignInTextView;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_person, container, false);
         initView(root);
+        initEvent(root);
         return root;
     }
 
@@ -25,9 +29,31 @@ public class PersonFragment extends Fragment implements View.OnClickListener {
     }
 
     private void initView(View root) {
+        mUserNameTextView = root.findViewById(R.id.user_name_person_textView);
+        mSignatureTextView = root.findViewById(R.id.signature_person_textView);
+        mSignOutStateTextView = root.findViewById(R.id.sign_out_state_person_textView);
+        mSignInTextView = root.findViewById(R.id.sign_in_person_textView);
+    }
+
+    private void initEvent(View root) {
         root.findViewById(R.id.like_person_constraintLayout).setOnClickListener(this);
         root.findViewById(R.id.post_person_constraintLayout).setOnClickListener(this);
         root.findViewById(R.id.settings_person_constraintLayout).setOnClickListener(this);
+
+        // 检查登录状态
+        if (false) {
+            // 登录
+            mUserNameTextView.setVisibility(View.VISIBLE);
+            mSignatureTextView.setVisibility(View.VISIBLE);
+            mSignOutStateTextView.setVisibility(View.GONE);
+            mSignInTextView.setVisibility(View.GONE);
+        } else {
+            // 未登录
+            mSignOutStateTextView.setVisibility(View.VISIBLE);
+            mSignInTextView.setVisibility(View.VISIBLE);
+            mUserNameTextView.setVisibility(View.GONE);
+            mSignatureTextView.setVisibility(View.GONE);
+        }
     }
 
     @Override
@@ -40,6 +66,8 @@ public class PersonFragment extends Fragment implements View.OnClickListener {
             case R.id.settings_person_constraintLayout:
                 Intent intent = new Intent(getActivity(), SettingsActivity.class);
                 startActivity(intent);
+                break;
+            case R.id.sign_in_person_textView:
                 break;
             default:
                 break;
