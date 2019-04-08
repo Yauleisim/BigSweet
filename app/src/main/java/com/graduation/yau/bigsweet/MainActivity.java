9a66eebdd9b27e7aad36cd4c1abcc90de14409ad
Yauleisim <13631445055@qq.com>
2019-04-08 17:02:18 +0800

    商城界面初步编写

--- app/src/main/java/com/graduation/yau/bigsweet/MainActivity.java
+++ app/src/main/java/com/graduation/yau/bigsweet/MainActivity.java
@@ -6,6 +6,8 @@ import android.support.v4.view.ViewPager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
 
 import com.graduation.yau.bigsweet.base.BaseActivity;
 
@@ -19,9 +21,19 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+        setStatusBar();
         initView();
         initEvent();
+    }
+
+    private void setStatusBar() {
+        if (getWindow() == null) {
+            return;
+        }
+        getWindow().setStatusBarColor(getResources().getColor(R.color.bg_bar_color));
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
     }
 
     protected void initView() {
